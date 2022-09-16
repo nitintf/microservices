9712a38986880e0104bfe43c7fe11c9176c3ab91
nitinpanwar08 <nitinpanwar0802@gmail.com>
2022-09-17 04:36:46 +0530

    testing api-gateway

--- packages/kafka/src/app.controller.ts
+++ packages/kafka/src/app.controller.ts
@@ -3,10 +3,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get('/authenticated')
   getHello() {
-    throw new UnauthorizedException()
+    throw new UnauthorizedException();
   }
 }
