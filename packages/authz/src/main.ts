8f36ad60da0baa370d33daa0388849b3333f70c9
nitinpanwar08 <nitinpanwar0802@gmail.com>
2022-09-21 01:03:25 +0530

    refactor: removing nestjs from authz
    will use go lang for auth service

--- packages/authz/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AuthzModule } from './authz.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AuthzModule);
-  await app.listen(3333);
-}
-bootstrap();
